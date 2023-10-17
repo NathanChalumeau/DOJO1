8d063ebcfb699c51df5d6c0dfecdf3d911488fe9
Nathan <nathan.chalumeau@gmail.com>
2023-10-17 15:40:33 +0200

    1000

--- src/utils/send-data.ts
+++ src/utils/send-data.ts
@@ -2,7 +2,7 @@ export const sendData = (kcal: number) => () => {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ kcal: kcal }),
+    body: JSON.stringify({ kcal: 1000 }),
   };
   fetch(`.netlify/functions/fauna`, requestOptions);
 };
